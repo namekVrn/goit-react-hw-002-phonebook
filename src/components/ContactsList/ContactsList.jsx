fef011bb7d104048f4692feeda833d86c865c99e
Andrey <gsmer.prof@yandex.ru>
2022-03-23 14:31:24 +0300

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -9,7 +9,7 @@ class Contacts extends Component{
                    {this.props.onListContacts.map((elem,id)=>{return (
                        <li className={css.listItem}key={id}>
                            <p className={css.name}>{elem.name}</p>
-                           <a className={css.contantsLink} href="tel:`${elem.tel}`">{elem.tel}</a>
+                           <a className={css.contantsLink}>{elem.tel}</a>
                            <p className={css.spam}>{elem.comment}</p>
                            <button type='button' onClick={()=>this.props.onDeleteTel(id)}>удалить</button>
                        </li>
