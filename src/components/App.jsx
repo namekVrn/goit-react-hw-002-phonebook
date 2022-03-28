7a40dd72df26af84d75d6ba67d9dabcd8d48d850
Andrey <gsmer.prof@yandex.ru>
2022-03-28 15:10:00 +0300

    renew

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { nanoid } from 'nanoid';
 import Form from './Form'
 import ContactsList from './ContactsList'
 import Filter from './Filter'
+import ModelDetails from './ModelDetails'
 import css from '../components/box.module.css'
 console.log(react)
 class App extends Component{
@@ -14,7 +15,21 @@ class App extends Component{
       {name: 'Annie Copeland', tel: '227-91-26', comment: 'spam'},
     ],
     filter: '',
+    showModal: false,
   }
+  componentDidUpdate(prevProps, prevState){ // метод вызывается после первого вызова render()
+    if(this.state.contacts !== prevState.contacts){
+        localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+  componentDidMount(){
+    if(this.state.contacts){
+      this.setState({
+        contacts: JSON.parse(localStorage.getItem('contacts'))
+      })
+    }
+    }
+    
   serchTel = (tel) => { // Проверка на совпадения номера в state
     let arrayNew = this.state.contacts
     return arrayNew.find(el=>el.tel === tel)
@@ -56,17 +71,29 @@ class App extends Component{
     console.log(normalizaFilter)
     return contacts.filter(contact=>contact.tel.includes(normalizaFilter))
   }
-
+  showModal = () => {
+    this.setState(({showModal}) =>({
+      showModal: !showModal
+    }))
+  }
 
   render(){
-    const {filter} = this.state
+    const {filter, showModal} = this.state
     return(
       <>
         <div className={css.box}>
+          <button type="button" onClick={this.showModal}>Открыть модалку</button>
+          {showModal && <ModelDetails onShowModal={this.showModal}>
+            <h1>Учи JS</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, pariatur.</p>
+            <button type="button" onClick={this.showModal}>Закрыть</button>
+          </ModelDetails>}
+
           <h2 className={css.titleTel}>Узнай кто звонил ? <br/>Если спам, добавь в базу номер!</h2>
           <Form onDataForm={this.formData} />
           <Filter value={filter} upDataFilterState={this.upDataFilter}/>
           <ContactsList onDeleteTel={this.deleteTel} onListContacts={this.getVisable()}/>
+        
         </div>
       </>
     )
